cc1bc207ed19c7ce3c4fe27c915c8f257bc18f69
Cheemx <chinmaymahajan999@gmail.com>
2024-08-11 13:33:13 +0530

    added user controller routes

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,12 @@ import { loginUser,
     registerUser, 
     refreshAccessToken,
     changeCurrentPassword,
-    getCurrentUser
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 } from "../controllers/user.js";
 import { upload } from "../middlewares/multer.js"
 import {verifyJWT} from "../middlewares/auth.js"
@@ -30,5 +35,13 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-pass").post(verifyJWT, changeCurrentPassword)
+router.route("current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
